f6f0cf32d32e040fd43cea810c6c31086ebbd2c5
Apoorav <medal.apoorav@gmail.com>
2023-07-01 19:00:24 +0530

    pkg fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,18 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
-// import Intro1 from './pages/Intro/Intro.jsx';
-// import CharExercise from './pages/SingleCharExercise/charExercise';
-import MiscTesting from './pages/Testing/MiscTest';
-
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* <Intro1 /> */}
-      {/* <CharExercise /> */}
-      {/* <MiscTesting /> */}
       <App />
     </BrowserRouter>
   </React.StrictMode>,
